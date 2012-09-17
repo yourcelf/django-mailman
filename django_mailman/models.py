d02d7f708c9be804233ce5e36a65c230ff09836d
Charlie DeTar <cfd@media.mit.edu>
2012-09-17 12:54:12 -0400

    Add method to change list settings
    Also, add an "owner" field to the List model which contains the email
    addresses of list owners.

--- django_mailman/models.py
+++ django_mailman/models.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 import re
+import sys
 import urllib2
 from types import UnicodeType
+from copy import deepcopy
+from email.utils import parseaddr
 
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+from django.conf import settings
 
 from webcall import MultipartPostHandler
 
@@ -104,6 +108,159 @@ UNSUBSCRIBE_DATA = {
     'unsubscribees_upload': None,
 }
 
+DEFAULT_LIST_SETTINGS = {
+    'general': dict(
+        admin_immed_notify="1",
+        admin_member_chunksize="30",
+        admin_notify_mchanges="0",
+        administrivia="1",
+        anonymous_list="0",
+        description="",
+        emergency="0",
+        first_strip_reply_to="0",
+        goodbye_msg="",
+        host_name="", #NOTE: must be filled by list
+        include_list_post_header=1,
+        include_rfc2369_headers=1,
+        info="",
+        max_days_to_hold="0",
+        max_message_size="40",
+        moderator="",
+        new_member_options=["ignore", "nodupes"],
+        owner="", #NOTE: must be filled by list
+        real_name="", # NOTE: must be filled by list.
+        reply_goes_to_list="0",
+        reply_to_address="",
+        respond_to_post_requests="1",
+        send_goodbye_msg="0",
+        send_reminders="0",
+        send_welcome_msg="0",
+        subject_prefix="", # NOTE: must be filled by list.
+        submit="Submit Your Changes",
+        umbrella_list="0",
+        umbrella_member_suffix="-owner",
+        welcome_msg="",
+    ),
+    'nondigest': dict(
+        nondigestable="1",
+        msg_header="",
+        msg_footer= """_______________________________________________
+%(real_name)s mailing list
+%(real_name)s@%(host_name)s
+%(web_page_url)slistinfo%(cgiext)s/%(_internal_name)s""",
+        scrub_nondigest="0",
+        regular_exclude_lists="",
+        regular_include_lists="",
+    ),
+    'digest': dict(
+        digestable="1",
+        digest_is_default="0", # 0: Regular, 1: Digest
+        mime_is_default_digest="0", # 0: Plain, 1: MIME
+        digest_size_threshold="30", #kb
+        digest_send_periodic="1",
+        digest_header="",
+        digest_footer="""_______________________________________________
+%(real_name)s mailing list
+%(real_name)s@%(host_name)s
+%(web_page_url)slistinfo%(cgiext)s/%(_internal_name)s""",
+        digest_volume_frequency="1", # 0: yearly, 1: monthly, 2: quarterly, 3: weekly, 4: daily
+    ),
+    'privacy/subscribing': dict(
+        advertised="1",
+        subscribe_policy="0",
+        unsubscribe_policy="0",
+        ban_list="",
+        private_roster="1",
+        obscure_addresses="1",
+    ),
+    'privacy/sender': dict(
+        default_member_moderation="0",
+        member_moderation_action="0",
+        member_moderation_notice="",
+        accept_these_nonmembers="",
+        hold_these_nonmembers="",
+        reject_these_nonmembers="",
+        discard_these_nonmembers="",
+        generic_nonmember_action="2",
+        forward_auto_discards="1",
+        nonmember_rejection_notice="",
+    ),
+    'privacy/recipient': dict(
+        require_explicit_destination="1",
+        acceptable_aliases="",
+        max_num_recipients="10",
+    ),
+    'privacy/spam': dict(
+        bounce_matching_headers="""# Lines that *start* with a '#' are comments.
+to: friend@public.com
+message-id: relay.comanche.denmark.eu
+from: list@listme.com
+from: .*@uplinkpro.com""",
+        hdrfilter_action_01="0",
+        hdrfilter_new_01="",
+        hdrfilter_rebox_01="",
+    ),
+    'bounce': dict(
+        bounce_processing="1",
+        bounce_score_threshold="5.0",
+        bounce_info_stale_after="7",
+        bounce_you_are_disabled_warnings="3",
+        bounce_you_are_disabled_warnings_interval="7",
+        bounce_unrecognized_goes_to_list_owner="1",
+        bounce_notify_owner_on_disable="1",
+        bounce_notify_owner_on_removal="1",
+    ),
+    "archive": dict(
+        archive="1",
+        archive_private="0",
+        archive_volume_frequency="1",
+    ),
+    "gateway": dict(
+        nntp_host="",
+        linked_newsgroup="",
+        gateway_to_news="0",
+        gateway_to_mail="0",
+        news_moderation="0",
+        news_prefix_subject_too="1",
+    ),
+    "autoreply": dict(
+        autorespond_postings="0",
+        autoresponse_postings_text="",
+        autorespond_admin="0",
+        autoresponse_admin_text="",
+        autorespond_requests="0",
+        autoresponse_request_text="",
+        autoresponse_graceperiod="90",
+    ),
+    "contentfilter": dict(
+        filter_content="0",
+        filter_mime_types="",
+        pass_mime_types="""multipart/mixed
+multipart/alternative
+text/plain""",
+        filter_filename_extensions="""exe
+bat
+cmd
+com
+pif
+scr
+vbs
+cpl""",
+        pass_filename_extensions="",
+        collapse_alternatives="1",
+        convert_html_to_plaintext="1",
+        filter_action="0",
+    ),
+    "topics": dict(
+        topics_enabled="0",
+        topics_bodylines_limit="5",
+        topic_box_01="",
+        topic_desc_01="",
+        topic_new_01="",
+        topic_rebox_01="",
+    )
+}
+
 def check_encoding(value, encoding):
     if isinstance(value, UnicodeType) and encoding != 'utf-8':
         value = value.encode(encoding)
@@ -113,10 +270,18 @@ def check_encoding(value, encoding):
 
 
 class List(models.Model):
-    name = models.CharField(max_length=50, unique=True)
-    password = models.CharField(max_length=50)
-    email = models.EmailField(unique=True)
-    main_url = models.URLField(verify_exists=False)
+    name = models.CharField(max_length=50, unique=True,
+        help_text="The real name of the list (the part before the @) with any desired case changes.")
+    password = models.CharField(max_length=50,
+        help_text="The list owner's password.")
+    email = models.EmailField(unique=True,
+        help_text="The full email address for this list, e.g. mylist@lists.example.com")
+    owner = models.TextField(
+        help_text="One or more email addresses of list owners, separated by \n."
+    )
+    main_url = models.URLField(verify_exists=False,
+        help_text="The URL to the mailman installation, e.g. http://lists.example.com/mailman/"
+    )
     encoding = models.CharField(max_length=20, choices=LANGUAGES)
 
     class Meta:
@@ -157,7 +322,9 @@ class List(models.Model):
         members = []
         letters = re.findall('letter=\w{1}', content)
         chunks = re.findall('chunk=\d+', content)
-        input = re.findall('name=".+_realname" type="TEXT" value=".*" size="[0-9]+" >', content)
+        input = re.findall(
+                'name=".+_realname" type="TEXT" value=".*" size="[0-9]+" >',
+                content)
         for member in input:
             info = member.split('" ')
             email = re.search('(?<=name=").+(?=_realname)', info[0]).group(0)
@@ -190,7 +357,8 @@ class List(models.Model):
 
         (msg, member) = self.__parse_status_content(unicode(content, self.encoding))
         if (msg not in SUBSCRIBE_MSG):
-            error = u'%s: %s' % (unicode(msg, encoding=self.encoding), unicode(member, encoding=self.encoding))
+            error = u'%s: %s' % (unicode(msg, encoding=self.encoding),
+                                 unicode(member, encoding=self.encoding))
             raise Exception(error.encode(self.encoding))
 
     def unsubscribe(self, email):
@@ -224,7 +392,8 @@ class List(models.Model):
             for chunk in chunks[1:]:
                 url_letter_chunk = "%s?%s&%s" %(url, letter, chunk)
                 content = opener.open(url_letter_chunk, data).read()
-                (letters, members, chunks) = self.__parse_member_content(content, self.encoding)
+                (letters, members, chunks) = self.__parse_member_content(
+                        content, self.encoding)
                 all_members.extend(members)
 
         members = {}
@@ -295,7 +464,8 @@ class List(models.Model):
 
     @classmethod
     def create_list(cls, main_url, email_domain, admin_pass, owner_email,
-                     owner_password, language='en', listname=u'', list_encoding='iso-8859-1'):
+                    owner_password, language='en', listname=u'',
+                    list_encoding='iso-8859-1'):
 
         url = '%s/create' % (main_url)
 
@@ -305,7 +475,7 @@ class List(models.Model):
 
         CREATE_DATA['listname'] = listname
         CREATE_DATA['owner'] = owner_email
-	CREATE_DATA['auth'] = admin_pass
+        CREATE_DATA['auth'] = admin_pass
         CREATE_DATA['password'] = owner_password
         CREATE_DATA['confirm'] = owner_password
         CREATE_DATA['langs'] = language
@@ -317,6 +487,29 @@ class List(models.Model):
             if len(re.findall(status, content)) > 0:
                 list_email = '%s@%s' % (listname, email_domain)
                 return cls(name=listname, password=owner_password, email=list_email, 
-                           main_url=main_url, encoding=list_encoding)
+                           main_url=main_url, owner=owner_email, encoding=list_encoding)
         raise Exception(content)
 	
+    def change_settings(self, user_settings):
+        new_settings = deepcopy(DEFAULT_LIST_SETTINGS)
+        new_settings['general']['real_name'] = self.name
+        new_settings['general']['subject_prefix'] = "[%s]" % self.name
+        new_settings['general']['host_name'] = parseaddr(self.email)[1].split("@")[1]
+        new_settings['general']['owner'] = self.owner
+
+        for key in user_settings:
+            new_settings[key].update(user_settings[key])
+
+        opener = urllib2.build_opener(MultipartPostHandler(self.encoding, True))
+        for key in user_settings:
+            if key not in DEFAULT_LIST_SETTINGS:
+                raise Exception("Unknown settings key: '%s'" % key)
+
+            url = '%s/admin/%s/%s' % (self.main_url, self.name, key)
+            post_data = {'adminpw': self.password} 
+            post_data.update(new_settings[key])
+            request = opener.open(url, post_data)
+            # TODO: try to identify error codes, and raise an exception if one
+            # is found.  The usual case is that nothing is returned.  Another
+            # strategy would be to parse the page form, and ensure its values
+            # match the posted ones.
